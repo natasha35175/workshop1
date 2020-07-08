f8be56643d5b3f747a270ad8bcfc0a199dc6635a
natasha35175 <nasro.yasuf@computingforall.org>
2020-07-08 21:22:58 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+let thisGameplay ="13"
+//string 
+console.log("way to go", thisGameplay) 
+
+let thisBookfly = "sky"
+//string
+console.log("flyhigh" , thisBookfly)
+
+let theRainstart = "wooshing"
+//string
+console.log("falldown" , theRainstart)
+
+let theTreefall = "ouch"
+//string
+console.log("treefell" , theTreefall)
+
+let theChildsmile = "wow"
+//string
+console.log("behappy" , theChildsmile)
+
+
+const soccerGame = 19;
+console.log("winwin", soccerGame) 
+//soccerGame = 19;
+//constant
+
+const playGround = 14 
+console.log("yeeeey" , playGround)
+//playground
+//constant
+
+const sky = 106
+console.log("blue" , sky)
+//iphone
+//constant
+
+const tree = 8
+console.log("green" , tree)
+//iphone
+//constant
+
+const iphone = 11 
+console.log("red" , iphone)
+//ipone
+//constant
